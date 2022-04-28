3588e09a7c20268cfb2079854172c1a1a1fbcfe1
bryan065 <30362590+bryan065@users.noreply.github.com>
2022-04-27 23:58:10 -0400

    Update keymap

--- keyboards/kprepublic/bm980hsrgb_v2/keymaps/vial-dookie/config.h
+++ keyboards/kprepublic/bm980hsrgb_v2/keymaps/vial-dookie/config.h
@@ -1,4 +1,16 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+  Copyright (c) 2022 Bryan065
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
@@ -12,23 +24,22 @@
 #define FORCE_NKRO
 #define LAYER_STATE_8BIT
 
+// Custom defines from /user/bryan065
 #define RGB_CUSTOM_TIMEOUT_DELAY 900000     // Timout delay in ms. Set to 15 minutes
 #define LOST_ARK
 #define BOOT_ANIM_X 112
 #define BOOT_ANIM_Y 0
 
-//RGB Matrix Effects
-#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-#define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
-
-#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#define ENABLE_RGB_MATRIX_SOLID_REACTIVE
-#define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-
-#define RGB_MATRIX_KEYPRESSES
-
-#define RGB_MATRIX_STARTUP_SPD 64
+/* Custom user specific features
+*
+*  Startup animation duration time and fade duration can be set here.
+*
+*  Defaults:
+*  STARTUP_ANIM_TIME = 2200
+*  FADE_TIME = 4    (Fast = 3 / Medium = 6 / Slow = 10)
+*/
+//#define STARTUP_ANIM_TIME 2200
+//#define FADE_TIME 4
 
 // Vial config
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
@@ -46,3 +57,18 @@
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
 #define NO_ACTION_ONESHOT
+
+//RGB Matrix Effects
+#define RGB_MATRIX_STARTUP_SPD 64
+
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+
+#define RGB_MATRIX_KEYPRESSES
+
+
--- keyboards/kprepublic/bm980hsrgb_v2/keymaps/vial-dookie/rules.mk
+++ keyboards/kprepublic/bm980hsrgb_v2/keymaps/vial-dookie/rules.mk
@@ -1,20 +1,21 @@
 DEBOUNCE_TYPE = asym_eager_defer_pk
 TAP_DANCE_ENABLE = yes
 
+# firmware size reduction
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+QMK_SETTINGS = no
+MOUSEKEY_ENABLE = no
+
+# via
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
-
-# firmware size reduction
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = no 
-QMK_SETTINGS = no
-
 COMBO_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
 
+# user
 USER_NAME := bryan065
-
 SRC += \
 	rgb.c\
 	bryan065.c
