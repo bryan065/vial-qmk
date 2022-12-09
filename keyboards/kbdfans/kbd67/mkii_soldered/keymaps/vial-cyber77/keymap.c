e2705df9dd42515536b214a6774868e0968ed1e4
bryan065 <le.bryan.065@gmail.com>
2022-12-08 21:07:08 -0500

    update keymap

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/vial-cyber77/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/vial-cyber77/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, LCTL(KC_BSPC), _______, KC_SCRL, _______, _______,
 		 KC_GRV, _______, _______, _______, _______, _______, _______,   KC_P7,   KC_P8,   KC_P9, _______, _______, _______,    LGUI(KC_PAUS), _______,
 		_______, _______, _______, _______, _______, _______, _______,   KC_P4,   KC_P5,   KC_P6, _______, _______,          KC_PENT,          KC_MYCM,
-		_______, _______, _______, _______, _______, _______, _______, _______,   KC_P1,   KC_P2,   KC_P3, _______, KC_NLCK,          KC_PGUP, KC_PSCR,
+		_______, _______, _______, _______, _______, _______, _______,   KC_P1,   KC_P2,   KC_P3, _______, _______, KC_NLCK,          KC_PGUP, KC_PSCR,
 		_______, _______, _______,          _______,          _______,            KC_P0,          KC_PDOT, _______,          KC_HOME, KC_PGDN, KC_END
   	),
 
